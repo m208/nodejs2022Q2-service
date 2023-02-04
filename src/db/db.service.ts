b731a98593a3d44f38986948459dc7fd0cdaab34
m208 <avcvet@yandex.ru>
2023-02-04 17:37:22 +0300

    feat: add favorites DB

--- src/db/db.service.ts
+++ src/db/db.service.ts
@@ -3,6 +3,7 @@ import { DBUsers } from './entities/DBUsers';
 import { DBTracks } from './entities/DBTracks';
 import { DBArtists } from './entities/DBArtists';
 import { DBAlbums } from './entities/DBAlbums';
+import { DBFavs } from './entities/DBFavs';
 
 @Injectable()
 export class DBInMemory {
@@ -10,4 +11,10 @@ export class DBInMemory {
   tracks = new DBTracks();
   artists = new DBArtists();
   albums = new DBAlbums();
+
+  favorites = {
+    track: new DBFavs(),
+    album: new DBFavs(),
+    artist: new DBFavs(),
+  };
 }
