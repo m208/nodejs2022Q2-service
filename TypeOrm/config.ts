9b9ef5b89849af80f6f919087285e4c80c446bdc
m208 <avcvet@yandex.ru>
2023-02-12 21:13:29 +0300

    fix: config db host

--- TypeOrm/config.ts
+++ TypeOrm/config.ts
@@ -1,5 +1,4 @@
 import { DataSource } from 'typeorm';
-import { ConfigService } from '@nestjs/config';
 import { config } from 'dotenv';
 import { Track } from '../src/routes/track/entities/track.entity';
 import { Artist } from '../src/routes/artist/entities/artist.entity';
@@ -14,21 +13,14 @@ import { InitialCreating1676222322081 } from './migrations/1676222322081-CreateD
 
 config();
 
-const configService = new ConfigService();
-
 export default new DataSource({
   type: 'postgres',
-  // host: process.env.POSTGRESS_HOST,
-  // port: +process.env.POSTGRESS_PORT,
-  // username: process.env.POSTGRES_USER,
-  // password: process.env.POSTGRESS_PASSWORD,
-  // database: process.env.POSTGRES_DB,
-  //host: configService.get('POSTGRES_HOST'),
-  host: '192.168.0.100',
-  port: configService.get('POSTGRES_PORT'),
-  username: configService.get('POSTGRES_USER'),
-  password: configService.get('POSTGRES_PASSWORD'),
-  database: configService.get('POSTGRES_DB'),
+  host: process.env.POSTGRESS_HOST,
+  port: +process.env.POSTGRESS_PORT,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRESS_PASSWORD,
+  database: process.env.POSTGRES_DB,
+
   entities: [
     Track,
     Artist,
